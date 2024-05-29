04f782997724225b2f9c38a9271f261b67f5b1f4
Liv-eth <jelrinkyer@gmail.com>
2024-05-29 16:33:24 +0100

    main

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(' this is cloud computing DEVOPS ; CI-CD PIPELINE !'));
+app.get('/', (req, res) => res.send(' NAME: JEL RINKYER APPOLLOS, MATRIC NUMBER: VUG/CSC/21/5968'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
